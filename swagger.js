e324e411f27f7c53375cb470a5c460ee36e44d8c
boluwatifeomotoyinbo <omotoyinbobade15@gmail.com>
2025-02-01 20:37:04 +0100

    swagger

--- swagger.js
+++ swagger.js
@@ -13,4 +13,9 @@ const doc = {
 const outputFile = './swagger-output.json';
 const endpointsFiles = ['./routes/index.js'];
 
-swaggerAutogen(outputFile, endpointsFiles, doc);
+console.log('Generating Swagger documentation...');
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+    console.log('Swagger documentation generated successfully.');
+}).catch((err) => {
+    console.error('Error generating Swagger documentation:', err);
+});
